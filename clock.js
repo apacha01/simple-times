0a1b8d0a2d36603f5eb1b8165f53018179972ee7
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-08-09 21:58:47 -0300

    Page working.

--- clock.js
+++ clock.js
@@ -1,3 +1,16 @@
-const dayjs = require('dayjs')
+// DOM constants
+const timezone = document.getElementById('timezone')
+const time = document.getElementById('time')
+const date = document.getElementById('date')
 
-console.log(dayjs().format());
+// Date - Time Functions
+const getTimezone = () => dayjs.tz.guess().replace('/', ' / ').replace('_', ' ')
+const getTime = () => dayjs().format('HH:mm:ss')
+const getDate = () => dayjs().format('dddd, D MMM, YYYY')
+const updateTimezone = () => timezone.innerText = getTimezone()
+const updateTime = () => setInterval(() => time.innerText = getTime(), 1000)
+const updateDate = () => setInterval(() => date.innerText = getDate(), 1000)
+
+updateTimezone()
+updateTime()
+updateDate()
