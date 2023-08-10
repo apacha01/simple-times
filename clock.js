52a68e9e1ce32b5e40bb64ada41944d7a6e6b52a
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-08-10 16:10:03 -0300

    Implemented change of timezones.

--- clock.js
+++ clock.js
@@ -3,12 +3,13 @@ const timezone = document.getElementById('timezone')
 const time = document.getElementById('time')
 const date = document.getElementById('date')
 const tzsOptions = document.getElementById('timezones-datalist')
+const btnApply = document.getElementById('apply-tz')
 const tzsPath = './node_modules/timezones-list/src/timezones.json';
 
 // Date - Time Functions
-const getTimezone = () => dayjs.tz.guess().replace('/', ' / ').replace('_', ' ')
-const getTime = () => dayjs().format('HH:mm:ss')
-const getDate = () => dayjs().format('dddd, D MMM, YYYY')
+const getTimezone = () => currTz.replace('/', ' / ').replace('_', ' ')
+const getTime = () => dayjs().tz(currTz).format('HH:mm:ss')
+const getDate = () => dayjs().tz(currTz).format('dddd, D MMM, YYYY')
 const updateTimezone = () => timezone.innerText = getTimezone()
 const updateTime = () => setInterval(() => time.innerText = getTime(), 1000)
 const updateDate = () => setInterval(() => date.innerText = getDate(), 1000)
@@ -24,6 +25,10 @@ const createTimezonesOptinos = () => {
 	})
 }
 
+// Aux
+let currTz = dayjs.tz.guess()
+
+// Main
 updateTimezone()
 updateTime()
 updateDate()
@@ -31,3 +36,11 @@ updateDate()
 MicroModal.init('modal-1')
 
 createTimezonesOptinos()
+
+btnApply.addEventListener('click', () => {
+	let t = document.getElementById('timezone-input').value
+	t = t.replace(' / ', '/').replace(' ', '_')
+	currTz = t
+	updateTimezone()
+	MicroModal.close('modal-1')
+});
