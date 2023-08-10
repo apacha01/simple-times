918ebfc2eaaffa2d5ec88cf166683fd7c10e2eba
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2023-08-10 15:57:08 -0300

    Added modal panel with micromodal npm module. Added timezones list with timezones-list npm module. Edited css and html acordingly.

--- clock.js
+++ clock.js
@@ -2,6 +2,8 @@
 const timezone = document.getElementById('timezone')
 const time = document.getElementById('time')
 const date = document.getElementById('date')
+const tzsOptions = document.getElementById('timezones-datalist')
+const tzsPath = './node_modules/timezones-list/src/timezones.json';
 
 // Date - Time Functions
 const getTimezone = () => dayjs.tz.guess().replace('/', ' / ').replace('_', ' ')
@@ -10,7 +12,22 @@ const getDate = () => dayjs().format('dddd, D MMM, YYYY')
 const updateTimezone = () => timezone.innerText = getTimezone()
 const updateTime = () => setInterval(() => time.innerText = getTime(), 1000)
 const updateDate = () => setInterval(() => date.innerText = getDate(), 1000)
+const createTimezonesOptinos = () => {
+	fetch(tzsPath)
+	.then(res => res.json())
+	.then(tzs => {
+		tzs.forEach(tz => {
+			let opt = document.createElement('option')
+			opt.value = tz.tzCode.replace('/', ' / ').replace('_', ' ')
+			tzsOptions.appendChild(opt)
+		})
+	})
+}
 
 updateTimezone()
 updateTime()
 updateDate()
+
+MicroModal.init('modal-1')
+
+createTimezonesOptinos()
